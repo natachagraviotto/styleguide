9d2a87f755937be7a578ae0cc97bfe1b47d2c163
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-05-16 18:14:57 +0200

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./Topbar.scss";
+
+function Topbar() {
+  return (
+    <>
+      <h1>Este es el Topbar</h1>
+    </>
+  );
+}
+
+export default Topbar;
---
+++ src/views/Styleguide/Styleguide.jsx
@@ -0,0 +1,13 @@
+import "./Styleguide.scss";
+
+function Styleguide() {
+  return (
+    <>
+      <h1>Guía de estilos</h1>
+
+      <div className="bg-color-lightblue circle">blue</div>
+    </>
+  );
+}
+
+export default Styleguide;
